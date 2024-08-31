ff4d42014c9eb3faac14e3e69f4415ccbdc721a1
YUSSIF ABDUL-RAHAMAN <abdulrahamanyussif62@gmail.com>
2024-08-31 21:25:55 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,33 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'GPA Goal Achiever',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          primarySwatch: Colors.green,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          textTheme: TextTheme(
+            headlineLarge: TextStyle(
+              fontSize: 28.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.green[800],
+            ),
+            headlineSmall: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.green[700],
+            ),
+            bodyMedium: TextStyle(
+              fontSize: 16.0,
+              color: Colors.black,
+            ),
+          ),
+          inputDecorationTheme: InputDecorationTheme(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12.0),
+              borderSide: BorderSide(color: Colors.green),
+            ),
+            labelStyle: TextStyle(color: Colors.green),
+          ),
+          elevatedButtonTheme:
+              ElevatedButtonThemeData(style: ElevatedButton.styleFrom())),
       home: LoginScreen(),
     );
   }
