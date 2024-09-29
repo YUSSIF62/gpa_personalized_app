e952808225403961ee005ff2d240199f2e830494
YUSSIF ABDUL-RAHAMAN <abdulrahamanyussif62@gmail.com>
2024-09-29 16:28:33 +0100

    Dropdowns

--- lib/screens/course_inputScreen.dart
+++ lib/screens/course_inputScreen.dart
@@ -5,8 +5,10 @@ class CoursesInputScreen extends StatefulWidget {
   final double studyHoursPerDay;
   final double studyHoursPerWeek;
 
-  CoursesInputScreen(
-      {required this.studyHoursPerDay, required this.studyHoursPerWeek});
+  CoursesInputScreen({
+    required this.studyHoursPerDay,
+    required this.studyHoursPerWeek,
+  });
 
   @override
   _CoursesInputScreenState createState() => _CoursesInputScreenState();
@@ -16,9 +18,21 @@ class _CoursesInputScreenState extends State<CoursesInputScreen> {
   final _formKey = GlobalKey<FormState>();
   String _courseName = '';
   double _creditHours = 0.0;
+  String _selectedYear = ''; // To store the selected university year
+  String _selectedSemester = ''; // To store the selected semester
 
   List<Map<String, dynamic>> _courses = [];
 
+  // Dropdown options for Year and Semester
+  final List<String> _years = [
+    'Level 100',
+    'Level 200',
+    'Level 300',
+    'Level 400'
+  ];
+  final List<String> _semesters = ['First Semester', 'Second Semester'];
+
+  // Method to add course details
   void _addCourse() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
@@ -35,8 +49,11 @@ class _CoursesInputScreenState extends State<CoursesInputScreen> {
     return _courses.fold(0.0, (total, course) => total + course['credits']);
   }
 
+  // Method to navigate to the Pledge Screen
   void _goToPledgeScreen() {
-    if (_courses.isNotEmpty) {
+    if (_courses.isNotEmpty &&
+        _selectedYear.isNotEmpty &&
+        _selectedSemester.isNotEmpty) {
       Navigator.push(
         context,
         MaterialPageRoute(
@@ -47,7 +64,9 @@ class _CoursesInputScreenState extends State<CoursesInputScreen> {
       );
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Please add at least one course')),
+        SnackBar(
+            content:
+                Text('Please complete all fields and add at least one course')),
       );
     }
   }
@@ -67,93 +86,163 @@ class _CoursesInputScreenState extends State<CoursesInputScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            // Title
-            Text(
-              'Enter your semester courses',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 20),
-            Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  // Input for Course Name
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: 'Course Name',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15),
-                        borderSide: BorderSide(color: Colors.green),
-                      ),
-                      filled: true,
-                      fillColor: Colors.grey[200],
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter a course name';
-                      }
-                      return null;
-                    },
-                    onSaved: (value) {
-                      _courseName = value!;
-                    },
-                  ),
-                  SizedBox(height: 15),
-
-                  // Input for Credit Hours
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: 'Credit hours',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15),
-                        borderSide: BorderSide(color: Colors.green),
-                      ),
-                      filled: true,
-                      fillColor: Colors.grey[200],
-                    ),
-                    keyboardType: TextInputType.number,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter the credit hours';
-                      }
-                      return null;
-                    },
-                    onSaved: (value) {
-                      _creditHours = double.parse(value!);
-                    },
-                  ),
-                  SizedBox(height: 20),
-
-                  // Add Course Button
-                  ElevatedButton(
-                    onPressed: _addCourse,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green,
-                      padding:
-                          EdgeInsets.symmetric(vertical: 15, horizontal: 100),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                    ),
-                    child: Text(
-                      'Add Courses',
-                      style: TextStyle(fontSize: 16, color: Colors.white),
-                    ),
-                  ),
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // Title
+              Text(
+                'Enter your Year and Semester Courses',
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green),
+                textAlign: TextAlign.center,
               ),
-            ),
-            SizedBox(height: 30),
+              SizedBox(height: 20),
 
-            // Course List (if available)
-            Expanded(
-              child: _courses.isEmpty
+              // Dropdown for Selecting University Year
+              DropdownButtonFormField<String>(
+                decoration: InputDecoration(
+                  labelText: 'Select your University Year',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(color: Colors.green),
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                ),
+                value: _selectedYear.isEmpty ? null : _selectedYear,
+                items: _years.map((String year) {
+                  return DropdownMenuItem<String>(
+                    value: year,
+                    child: Text(year),
+                  );
+                }).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    _selectedYear = value!;
+                  });
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please select your university year';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 15),
+
+              // Dropdown for Selecting Semester
+              DropdownButtonFormField<String>(
+                decoration: InputDecoration(
+                  labelText: 'Select your Semester',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(color: Colors.green),
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                ),
+                value: _selectedSemester.isEmpty ? null : _selectedSemester,
+                items: _semesters.map((String semester) {
+                  return DropdownMenuItem<String>(
+                    value: semester,
+                    child: Text(semester),
+                  );
+                }).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    _selectedSemester = value!;
+                  });
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please select your semester';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+
+              // Form for Adding Course Name and Credit Hours
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    // Input for Course Name
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Course Name',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          borderSide: BorderSide(color: Colors.green),
+                        ),
+                        filled: true,
+                        fillColor: Colors.grey[200],
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a course name';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _courseName = value!;
+                      },
+                    ),
+                    SizedBox(height: 15),
+
+                    // Input for Credit Hours
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Credit Hours',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          borderSide: BorderSide(color: Colors.green),
+                        ),
+                        filled: true,
+                        fillColor: Colors.grey[200],
+                      ),
+                      keyboardType: TextInputType.number,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter the credit hours';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _creditHours = double.parse(value!);
+                      },
+                    ),
+                    SizedBox(height: 20),
+
+                    // Add Course Button
+                    ElevatedButton(
+                      onPressed: _addCourse,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        padding:
+                            EdgeInsets.symmetric(vertical: 15, horizontal: 100),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                      child: Text(
+                        'Add Course',
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 30),
+
+              // Course List (if available)
+              _courses.isEmpty
                   ? Center(child: Text('No courses added yet.'))
                   : ListView.builder(
+                      shrinkWrap: true,
                       itemCount: _courses.length,
                       itemBuilder: (context, index) {
                         return ListTile(
@@ -163,33 +252,33 @@ class _CoursesInputScreenState extends State<CoursesInputScreen> {
                         );
                       },
                     ),
-            ),
 
-            // Total Courses and Credits
-            SizedBox(height: 10),
-            Text(
-              'Total Courses: ${_courses.length}, Total Credits: $_totalCredits',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 20),
+              // Total Courses and Credits
+              SizedBox(height: 20),
+              Text(
+                'Total Courses: ${_courses.length}, Total Credits: $_totalCredits',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 20),
 
-            // Next Button
-            ElevatedButton(
-              onPressed: _goToPledgeScreen,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.green,
-                padding: EdgeInsets.symmetric(vertical: 15),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
+              // Next Button
+              ElevatedButton(
+                onPressed: _goToPledgeScreen,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                child: Text(
+                  'Next',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
                 ),
               ),
-              child: Text(
-                'Next',
-                style: TextStyle(fontSize: 18, color: Colors.white),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -10,32 +10,53 @@ class _HomePageScreenState extends State<HomePageScreen> {
   final _formKey = GlobalKey<FormState>();
   double _currentGPA = 0.0;
   double _goalGPA = 0.0;
-  String _selectedStudyHabit = '';
   double _studyHoursPerDay = 0.0;
   double _studyHoursPerWeek = 0.0;
+  List<String> _selectedStudyHabits =
+      []; // Track multiple selected study habits
 
   final List<String> _studyHabits = [
-    'Study regularly',
-    'Last-minute cramming',
-    'Group study',
-    'Using flashcards',
-    'Taking notes during lectures',
-    'Watching tutorial videos',
-    'Attending study groups',
-    'Self-paced learning'
+    'Reading',
+    'Taking Notes',
+    'Group Study',
+    'Using Flashcards',
+    'Taking Notes During Lectures',
+    'Watching Tutorial Videos',
+    'Attending Study Groups',
+    'Self-Paced Learning',
+    'Using Mnemonics',
+    'Practicing with Past Papers',
+    'Explaining Concepts to Others',
+    'Creating Mind Maps',
+    'Setting Study Goals',
+    'Reviewing Regularly',
+    'Using Study Apps',
+    'Active Recall',
+    'Practice Testing',
+    'Summarizing Information'
   ];
 
+  // Adjusted to include a validation check for _selectedStudyHabits
   void _submitDetails() {
     if (_formKey.currentState!.validate()) {
-      _formKey.currentState!.save();
-      Navigator.push(
-        context,
-        MaterialPageRoute(
+      // Check if at least one study habit is selected
+      if (_selectedStudyHabits.isNotEmpty) {
+        _formKey.currentState!.save();
+        Navigator.push(
+          context,
+          MaterialPageRoute(
             builder: (context) => CoursesInputScreen(
-                  studyHoursPerDay: _studyHoursPerDay,
-                  studyHoursPerWeek: _studyHoursPerWeek,
-                )),
-      );
+              studyHoursPerDay: _studyHoursPerDay,
+              studyHoursPerWeek: _studyHoursPerWeek,
+            ),
+          ),
+        );
+      } else {
+        // Show a SnackBar if no study habit is selected
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Please select at least one study habit.')),
+        );
+      }
     }
   }
 
@@ -56,150 +77,145 @@ class _HomePageScreenState extends State<HomePageScreen> {
         padding: const EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              // Welcome Message
-              Text(
-                'Welcome',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 10),
-              RichText(
-                text: TextSpan(
-                  text: "Let's plan your ",
-                  style: TextStyle(fontSize: 20, color: Colors.black),
-                  children: <TextSpan>[
-                    TextSpan(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                // Welcome Message
+                Text(
+                  'Welcome',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 10),
+                RichText(
+                  text: TextSpan(
+                    text: "Let's plan your ",
+                    style: TextStyle(fontSize: 20, color: Colors.black),
+                    children: <TextSpan>[
+                      TextSpan(
                         text: 'CGPAgoal',
                         style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.green,
-                            fontWeight: FontWeight.bold)),
-                  ],
-                ),
-              ),
-              SizedBox(height: 30),
-
-              // Input for Current CGPA
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Enter your current CGPA',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                    borderSide: BorderSide(color: Colors.green),
-                  ),
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                ),
-                keyboardType: TextInputType.number,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your current CGPA';
-                  }
-                  return null;
-                },
-                onSaved: (value) => _currentGPA = double.parse(value!),
-              ),
-              SizedBox(height: 15),
-
-              // Input for Targeted CGPA
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Enter your Targeted CGPA',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                    borderSide: BorderSide(color: Colors.green),
-                  ),
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                ),
-                keyboardType: TextInputType.number,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your targeted CGPA';
-                  }
-                  return null;
-                },
-                onSaved: (value) => _goalGPA = double.parse(value!),
-              ),
-              SizedBox(height: 15),
-
-              // Dropdown for Study Habits
-              DropdownButtonFormField<String>(
-                decoration: InputDecoration(
-                  hintText: 'Select study habits',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                    borderSide: BorderSide(color: Colors.green),
-                  ),
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                ),
-                value: _selectedStudyHabit.isEmpty ? null : _selectedStudyHabit,
-                items: _studyHabits.map((String habit) {
-                  return DropdownMenuItem<String>(
-                    value: habit,
-                    child: Text(habit),
-                  );
-                }).toList(),
-                onChanged: (value) {
-                  setState(() {
-                    _selectedStudyHabit = value!;
-                  });
-                },
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please select a study habit';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _selectedStudyHabit = value!;
-                },
-              ),
-              SizedBox(height: 15),
-
-              // Input for Study Hours Per Week
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Study Hours Per Week',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                    borderSide: BorderSide(color: Colors.green),
-                  ),
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                ),
-                keyboardType: TextInputType.number,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your study hours per week';
-                  }
-                  return null;
-                },
-                onSaved: (value) => _studyHoursPerWeek = double.parse(value!),
-              ),
-
-              SizedBox(height: 30),
-
-              // Next Button
-              ElevatedButton(
-                onPressed: _submitDetails,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.green,
-                  padding: EdgeInsets.symmetric(vertical: 15),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
+                          fontSize: 20,
+                          color: Colors.green,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                child: Text(
-                  'Next',
-                  style: TextStyle(fontSize: 18, color: Colors.white),
+                SizedBox(height: 30),
+
+                // Input for Current CGPA
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter your current CGPA',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide(color: Colors.green),
+                    ),
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                  ),
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your current CGPA';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _currentGPA = double.parse(value!),
                 ),
-              ),
-            ],
+                SizedBox(height: 15),
+
+                // Input for Targeted CGPA
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter your Targeted CGPA',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide(color: Colors.green),
+                    ),
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                  ),
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your targeted CGPA';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _goalGPA = double.parse(value!),
+                ),
+                SizedBox(height: 15),
+
+                // Grid for Multiple Study Habit Selection
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Wrap(
+                    spacing: 10.0,
+                    runSpacing: 10.0,
+                    children: _studyHabits.map((habit) {
+                      final isSelected = _selectedStudyHabits.contains(habit);
+                      return FilterChip(
+                        label: Text(habit),
+                        selected: isSelected,
+                        onSelected: (bool selected) {
+                          setState(() {
+                            if (selected) {
+                              _selectedStudyHabits.add(habit);
+                            } else {
+                              _selectedStudyHabits.remove(habit);
+                            }
+                          });
+                        },
+                        selectedColor: Colors.green,
+                      );
+                    }).toList(),
+                  ),
+                ),
+                SizedBox(height: 15),
+
+                // Input for Study Hours Per Week
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Study Hours Per Week',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide(color: Colors.green),
+                    ),
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                  ),
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your study hours per week';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _studyHoursPerWeek = double.parse(value!),
+                ),
+                SizedBox(height: 30),
+
+                // Next Button
+                ElevatedButton(
+                  onPressed: _submitDetails,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  child: Text(
+                    'Next',
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
